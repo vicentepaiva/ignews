15dd355ce37f898e6f7258dcba4ee50c2bfdb3f4
vicentepaiva <vicentepaivadev@gmail.com>
2022-03-02 11:33:38 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
+import {SessionProvider} from 'next-auth/react';
+
+
+
+import '../styles/global.scss';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+    <Header/>
+    <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
+
+export default MyApp
